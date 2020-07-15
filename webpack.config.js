26a0f370f083fa71ebe866ceecba7692defe4e79
root <root@ip-172-31-5-249.us-east-2.compute.internal>
2020-07-15 06:05:19 +0000

    added

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,19 @@ module.exports = {
 	filename: 'main.js',
 	path : path.resolve(__dirname, 'dist')
     },
+
+
+
+performance: { hints: false,
+ maxEntrypointSize: 512000,
+ maxAssetSize: 512000
+},
+
+
+
+
+
+
     resolve: {
 		extensions: [ '.js', '.jsx', 'png','css' ]
 	},
